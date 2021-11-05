6452957d40ba1bf751484852b88588fbca3269df
tomazmm <tomaz.mesarec@gmail.com>
2021-11-05 15:26:02 +0100

    Minting script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,39 +1,44 @@
 import base64
 from terra_sdk.client.localterra import LocalTerra
 from terra_sdk.core import Coins
-from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract, MsgExecuteContract
+from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract
 from terra_sdk.core.auth.data.tx import StdFee
 
-
 lt = LocalTerra()
-test1 = lt.wallets["test1"]
+owner = lt.wallets["test1"]
+
 
 def store() -> int:
     contract_file = open("../artifacts/cw721_metadata_onchain.wasm", "rb")
     file_bytes = base64.b64encode(contract_file.read()).decode()
-    store_code = MsgStoreCode(test1.key.acc_address, file_bytes)
-    store_code_tx = test1.create_and_sign_tx(msgs=[store_code], fee=StdFee(10000000, "1000000uluna"))
+    store_code = MsgStoreCode(owner.key.acc_address, file_bytes)
+    store_code_tx = owner.create_and_sign_tx(msgs=[store_code], fee=StdFee(10000000, "1000000uluna"))
     return int(lt.tx.broadcast(store_code_tx).logs[0].events_by_type["store_code"]["code_id"][0])
 
 
-def instantiate(code_id: int):
-    instantiate = MsgInstantiateContract(
-      test1.key.acc_address,
-      test1.key.acc_address,
+def instantiate(code_id: int) -> str:
+    init = MsgInstantiateContract(
+      owner.key.acc_address,
+      owner.key.acc_address,
       code_id,
       {
         "name": "ArtsyApes",
         "symbol": "APE",
-        "minter": str(test1.key.acc_address)
+        "minter": str(owner.key.acc_address)
       },
       Coins({"uluna": 10000000}),
     )
-    instantiate_tx = test1.create_and_sign_tx(msgs=[instantiate])
+    instantiate_tx = owner.create_and_sign_tx(msgs=[init])
     instantiate_tx_result = lt.tx.broadcast(instantiate_tx)
-    contract_addresss = instantiate_tx_result.logs[0].events_by_type['instantiate_contract']['contract_address'][0]
-    print(f"Contract address: {contract_addresss}")
+    return instantiate_tx_result.logs[0].events_by_type['instantiate_contract']['contract_address'][0]
+
+
+def deploy():
+    code_id = store()
+    contract_address = instantiate(code_id)
+    print(f"Code ID : {code_id}\nContract address: {contract_address}")
+    return code_id, contract_address
 
 
 if __name__ == '__main__':
-    code_id = store()
-    instantiate(code_id)
+    deploy()
---
+++ scripts/mint.py
@@ -0,0 +1,37 @@
+import pprint
+import random
+from terra_sdk.core import AccAddress, Coins
+from terra_sdk.core.auth import StdFee
+from terra_sdk.core.broadcast import BlockTxBroadcastResult
+
+from scripts.deploy import deploy, owner, lt
+from terra_sdk.core.wasm import MsgExecuteContract
+
+
+def mint(contract_address: str):
+    mint_msg = MsgExecuteContract(
+        owner.key.acc_address,
+        AccAddress(contract_address),
+        {
+            "mint": {
+                "token_id": str(random.randint(1, 1000000)),
+                "owner": owner.key.acc_address,
+                "token_uri": "www.ipfs_link"
+            }
+        }
+    )
+    mint_tx = owner.create_and_sign_tx(msgs=[mint_msg], fee=StdFee(1000000, Coins(uluna=1000000)))
+    mint_tx_result = lt.tx.broadcast(mint_tx)
+    print_tx_result(mint_tx_result)
+
+
+def print_tx_result(tx_result: BlockTxBroadcastResult):
+    print(f"Height: {tx_result.height}")
+    print(f"TxHash: {tx_result.txhash}")
+    for event in tx_result.logs[0].events:
+        print(f"{event['type']} : {pprint.pformat(event['attributes'])}")
+
+
+if __name__ == '__main__':
+    _, address = deploy()
+    mint(address)
